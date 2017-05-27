dbe8c419e4868c9b59f295c65e4b651cad859f2a
ertugrulungor <ertugrulungor@gmail.com>
2017-05-27 18:11:54 +0300

    mvc projesi güncellendi

--- hafta-12/turbemblog/Controllers/AdminController.cs
+++ hafta-12/turbemblog/Controllers/AdminController.cs
@@ -9,37 +9,9 @@ namespace turbemblog.Controllers
 {
     public class AdminController : Controller
     {
-        turbemblogEntities db = new turbemblogEntities();
-        // GET: Admin
         public ActionResult Index()
         {
             return View();
-        }
-
-        public JsonResult GirisYap(string kulAdi, string sifre)
-        {
-            if(db.Kullanicis.Any(x=>x.kullaniciAdi == kulAdi && x.sifre == sifre))
-            {
-                Kullanici k = db.Kullanicis.FirstOrDefault(x => x.kullaniciAdi == kulAdi && x.sifre == sifre);
-
-                var jsonObject = new
-                {
-                    username = k.kullaniciAdi,
-                    email = k.email,
-                    success = 1
-                };
-                return Json(jsonObject, JsonRequestBehavior.AllowGet);
-            }
-            else
-            {
-                var jsonObject = new
-                {
-                    username = "",
-                    email = "",
-                    success = 0
-                };
-                return Json(jsonObject, JsonRequestBehavior.AllowGet);
-            }
         }
     }
 }
